ba7a4cdfd65d512dc3940fbda614dab9feed2431
GitFlowers <gusrn160304@gmail.com>
2021-10-13 16:07:36 +0900

    fix(RacingCar): 이동거리를 '-' 로 표시하기위한 프로퍼티 추가

--- src/main/java/racinggame/domain/RacingCar.java
+++ src/main/java/racinggame/domain/RacingCar.java
@@ -1,8 +1,13 @@
 package racinggame.domain;
 
+import nextstep.utils.Randoms;
+
 public class RacingCar {
+	
+	
 	private String name;
 	private int mileage = 0;
+	private StringBuilder mileageStr = new StringBuilder();
 	
 	public RacingCar(String name) {
 		this.name = name;
@@ -14,5 +19,18 @@ public class RacingCar {
 	
 	public int getMileage() {
 		return this.mileage;
+	}
+	
+	public String getMileageStr() {
+		return this.mileageStr.toString();
+	}
+	
+	public void moveOneStep() {
+		mileage += 1;
+		mileageStr.append("-");
+	}
+	
+	public void move() {
+		
 	}
 }
